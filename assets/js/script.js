e3ca820d9b83e0d731d922cad313d7c1010a097a
AngerOverApathy <angersavestheworld@gmail.com>
2022-02-08 20:17:14 -0800

    added variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
 var buttonPress = document.getElementById('rouletteBtn')
 
 var restaurants = []
+var map;
+var service;
 
 function initMap() {
     var center= { lat: 38.575764, lng: -121.478851 };
-    var map = new google.maps.Map(document.getElementById("container1"), {
+    map = new google.maps.Map(document.getElementById("container1"), {
     center: center,
     zoom: 15
     });
@@ -15,14 +17,14 @@ function initMap() {
         query: "mexican chinese italian restaurant"
     };
 
-      var service = new google.maps.places.PlacesService(map);
+    service = new google.maps.places.PlacesService(map);
 
       service.textSearch(request, function(results, status) {
           console.log(status)
         if (status === google.maps.places.PlacesServiceStatus.OK) {
-            // results.forEach(function(item) {
-            //     console.log(`restaurant is: ${item.name} price is: ${item.price_level} photo is: ${item.photos} address is: ${item.formatted_address}`)
-            // })
+            results.forEach(function(item) {
+                console.log(`restaurant is: ${item.name} price is: ${item.price_level} photo is: ${item.photos} address is: ${item.formatted_address}`)
+            })
           console.log(results[Math.floor(Math.random()*results.length)])
           restaurants = results
         }
@@ -33,3 +35,6 @@ buttonPress.addEventListener('click', function () {
     initMap ()
     console.log(restaurants[Math.floor(Math.random()*restaurants.length)])
 })
+
+//when roulette btn pushed, call service text search
+
