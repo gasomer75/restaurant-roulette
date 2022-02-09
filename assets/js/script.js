875f0a0a6345aecf53e16f6e6b6c6efd3ddd5be7
AngerOverApathy <angersavestheworld@gmail.com>
2022-02-08 20:10:14 -0800

    console logging button press

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+var buttonPress = document.getElementById('rouletteBtn')
+
+var restaurants = []
+
 function initMap() {
     var center= { lat: 38.575764, lng: -121.478851 };
     var map = new google.maps.Map(document.getElementById("container1"), {
@@ -8,7 +12,7 @@ function initMap() {
     var request = {
         location: center,
         radius: '500',
-        query: "mexican chinese restaurant"
+        query: "mexican chinese italian restaurant"
     };
 
       var service = new google.maps.places.PlacesService(map);
@@ -16,8 +20,16 @@ function initMap() {
       service.textSearch(request, function(results, status) {
           console.log(status)
         if (status === google.maps.places.PlacesServiceStatus.OK) {
-          console.log(results)
+            // results.forEach(function(item) {
+            //     console.log(`restaurant is: ${item.name} price is: ${item.price_level} photo is: ${item.photos} address is: ${item.formatted_address}`)
+            // })
+          console.log(results[Math.floor(Math.random()*results.length)])
+          restaurants = results
         }
       });
 }
 
+buttonPress.addEventListener('click', function () {
+    initMap ()
+    console.log(restaurants[Math.floor(Math.random()*restaurants.length)])
+})
